7532ec605d70385e84e049a04a3880b68d4ee474
Rainer Sigwald <raines@microsoft.com>
2020-12-21 14:30:18 -0600

    ??

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+    if (github.context.eventName !== 'pull_request_review') {
+        core.setFailed('Called on wrong type')
+
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+
+        return;
+    }
+
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
